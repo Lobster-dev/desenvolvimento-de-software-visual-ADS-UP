47720e0a935cebc7b3fd7a5293dfdca10258970b
Lobster-dev <80717591+Lobster-dev@users.noreply.github.com>
2022-08-02 11:46:19 -0300

    Update Program.cs

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -6,7 +6,11 @@ namespace ConsoleApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            
+            Console.WriteLine("Digite um numero: ");
+            string numero1 = Console.ReadLine();
+            Console.WriteLine("O numero digitado foi : " + numero1);
         }
     }
 }
